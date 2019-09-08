cda98da81a57611dc2f10a51015dabbae2a1da17
saigal_amatya <saigal@noveltytechnology.com>
2019-09-08 20:08:08 +0545

    auth state & private routes

--- leadmanager/frontend/src/components/layout/Header.js
+++ leadmanager/frontend/src/components/layout/Header.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-sm navbar-light bg-light">
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <a className="navbar-brand" href="#">Lead Manager</a>
-          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-
-          </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
+        <div className="container">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a className="navbar-brand" href="#">Lead Manager</a>
+            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+              <li className="nav-item">
+                <Link to='/register' className="nav-item">
+                  Register
+              </Link>
+              </li>
+              <li className="nav-item">
+                <Link to='/login' className="nav-item">
+                  Login
+              </Link>
+              </li>
+            </ul>
+            <form className="form-inline my-2 my-lg-0">
+              <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </div>
         </div>
       </nav>
     )
